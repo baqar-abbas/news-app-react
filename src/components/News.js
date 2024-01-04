fcbd11224c836c338a11359db2ea901570c0ab7f
baqar-abbas <baqar007@hotmail.com>
2024-01-04 07:12:34 +0500

    fetch data from news api and display data in the ui.

--- src/components/News.js
+++ src/components/News.js
@@ -2,31 +2,36 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 
 export class News extends Component {
+    constructor(){
+        super();
+        console.log("Hello I am a constructor from News")
+        this.state = {
+            articles: [],
+            loading: false
+        }
+    }
+
+    async componentDidMount(){
+        console.log("Component Did Mount");
+        let url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=6389d6af32b0408c8c65427f0470f1cb"
+        let data = await fetch(url);
+        
+        let parsedData = await data.json();   
+        console.log(parsedData);
+        this.setState({articles: parsedData.articles})
+     }
   render() {
     return (
       <div className="container my-3">
-        <h2>News App - Top Headlines</h2>
-        <div className="row">
-            <div className="col-md-4">
-        <NewsItem title="My Title" description="My Description"/>
-        </div>
-        <div className="col-md-4">
-        <NewsItem title="My Title" description="My Description"/>
-        </div>
-        <div className="col-md-4">
-        <NewsItem title="My Title" description="My Description"/>
-        </div>
-        </div>
+        <h2 className="heading">News App - Top Headlines</h2>
+        
         <div className="row my-3">
-        <div className="col-md-4">
-        <NewsItem title="My Title" description="My Description"/>
-        </div>
-        <div className="col-md-4">
-        <NewsItem title="My Title" description="My Description"/>
-        </div>
-        <div className="col-md-4">
-        <NewsItem title="My Title" description="My Description"/>
-        </div>
+        {this.state.articles.map((element) => {
+            return (<div key={element.url} className="col-md-4">
+            <NewsItem title={element.title?element.title:''} description={element.description?element.description:''} imageUrl = {element.urlToImage} newsUrl={element.url}/>
+            </div>);
+        })}
+            
         </div>
       </div>
     )
