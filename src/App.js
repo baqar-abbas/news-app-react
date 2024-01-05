1fa590a109f73cd949154558a0ac652fc3e8c885
baqar-abbas <baqar007@hotmail.com>
2024-01-06 04:08:34 +0500

    implement the logic of fetching news category wise. Routes defined for router.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import {Routes, Route} from 'react-router-dom';
 
 import React, { Component } from 'react'
 import Navbar from './components/Navbar';
@@ -10,7 +11,16 @@ export default class App extends Component {
     return (
       <div>
         <Navbar/>
-        <News pageSize = {12}/>
+        {/* <News pageSize = {12} country="us" category="sports"/> */}
+        <Routes>
+          <Route exact path="/" element={<News key="general" pageSize = {12} country="us" category="general"/>}/>
+          <Route exact path="/business" element={<News key="business" pageSize = {12} country="us" category="business"/>}/>
+          <Route exact path="/entertainment" element={<News key="entertainment" pageSize = {12} country="us" category="entertainment"/>}/>
+          <Route exact path="/health" element={<News key="health" pageSize = {12} country="us" category="health"/>}/>
+          <Route exact path="/science" element={<News key="science" pageSize = {12} country="us" category="science"/>}/>
+          <Route exact path="/sports" element={<News key="sports" pageSize = {12} country="us" category="sports"/>}/>
+          <Route exact path="/technology" element={<News key="technology" pageSize = {12} country="us" category="technology"/>}/>
+        </Routes>
       </div>
     )
   }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 export class Navbar extends Component {
 //   static propTypes = {
@@ -10,29 +11,43 @@ export class Navbar extends Component {
       <div>
         <nav className="navbar bg-dark navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
   <div className="container-fluid">
-    <a className="navbar-brand" href="/">News App</a>
+    <Link className="navbar-brand" to="/">News App</Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="/">Home</a>
+          <Link className="nav-link active" aria-current="page" to="/">Home</Link>
         </li>
+        {/* <li className="nav-item">
+          <Link className="nav-link active" to="/">About</Link>
+        </li> */}
         <li className="nav-item">
-          <a className="nav-link" href="/">About</a>
-        </li>
-        <li className="nav-item dropdown">
-          <a className="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          <Link className="nav-link active" to="/business">Business</Link></li>
+        <li className="nav-item">
+          <Link className="nav-link active" to="/entertainment">Entertainment</Link></li>
+        {/* <li className="nav-item">
+          <Link className="nav-link active" to="/general">General</Link></li> */}
+        <li className="nav-item">
+          <Link className="nav-link active" to="/health">Health</Link></li>
+        <li className="nav-item">
+          <Link className="nav-link active" to="/science">Science</Link></li>
+        <li className="nav-item">
+          <Link className="nav-link active" to="/sports">Sports</Link></li>
+        <li className="nav-item">
+          <Link className="nav-link active" to="/technology">Technology</Link></li>
+        {/* <li className="nav-item dropdown">
+          <a className="nav-link dropdown-toggle" to="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Dropdown
           </a>
           <ul className="dropdown-menu">
-            <li><a className="dropdown-item" href="/">Action</a></li>
-            <li><a className="dropdown-item" href="/">Another action</a></li>
+            <li><a className="dropdown-item" to="/">Action</a></li>
+            <li><a className="dropdown-item" to="/">Another action</a></li>
             <li><hr className="dropdown-divider"/></li>
-            <li><a className="dropdown-item" href="/">Something else here</a></li>
+            <li><a className="dropdown-item" to="/">Something else here</a></li>
           </ul>
-        </li>
+        </li> */}
       </ul>
     </div>
   </div>
